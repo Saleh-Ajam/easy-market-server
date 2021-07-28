5c22bcf4e8285a644cc29f42fe9e2e2d9ecd2c11
Saleh <saleh-aj@hotmail.com>
2021-07-27 21:23:02 -0700

    Express Session Part 2 Sinup,Login and Logout a user

--- app.js
+++ app.js
@@ -39,39 +39,24 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if(!req.session.user){
-    var authHeader = req.headers.authorization;
-    if(!authHeader){
       var err = new Error('You are not authenticated!');
       res.setHeader('www-Authenticate','Basic');
       err.status = 401;
       next(err);
-    }
 
-    var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-    // console.log('username: ' + username);
-    // console.log('password: '+ password);
-
-    if (username == 'admin' && password == 'password'){
-      req.session.user ='admin';
-      next();
-    }else{
-      var err = new Error('The username or password is incorrect');
-      res.setHeader('www-Authenticate','Basic');
-      err.status = 401;
-      next(err);
-    }
   }else{
-    if(req.session.user === 'admin'){
+    if(req.session.user === 'authenticated'){
       next();
     }else{
       var err = newError('You are not authenticated!');
-      err.status = 401;
+      err.status = 403;  // The HTTP 403 Forbidden client error status response code indicates that the server understood the request but refuses to authorize it
       return next(err);
     }
   }
@@ -80,8 +65,7 @@ function auth(req, res, next) {
 }
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
