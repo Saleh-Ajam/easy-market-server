ae390d1ed97f8fc0fdae1cb5fa074c7320f54bff
Saleh <saleh-aj@hotmail.com>
2021-08-05 14:12:51 -0700

    Make Firstname & Lastname for the User Required to Signup

--- models/user.js
+++ models/user.js
@@ -6,11 +6,11 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
     firstname:{
         type: String,
-        default: ''
+        required : true
     },
     lastname:{
         type: String,
-        default: ''
+        required : true
     },
     admin: {
         type: Boolean,
--- routes/users.js
+++ routes/users.js
@@ -23,18 +23,12 @@ router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.veri
 
 router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   // the mongoose plugin provide us with a method called register on the user schema and model
-  User.register( new User({username: req.body.username}), req.body.password,(err, user) =>{
+  User.register( new User({username: req.body.username ,lastname:req.body.lastname ,firstname: req.body.firstname}), req.body.password,(err, user) =>{
     if(err){
       res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json');
       res.json({err: err})
     }else{
-      if(req.body.firstname){
-        user.firstname = req.body.firstname;
-      } 
-      if(req.body.lastname){
-        user.lastname = req.body.lastname;
-      }
       user.save((err, user) => {
         if(err){
           res.statusCode = 500;
